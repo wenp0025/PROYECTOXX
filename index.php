ff74f041889d543afde215c95e95cf5da33cc703
wendy <wendy@DESKTOP-EENDUFP>
2021-03-02 12:53:37 -0600

    Create index

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        header("Location: listarr.php");
         ?>
+        
     </body>
 </html>
